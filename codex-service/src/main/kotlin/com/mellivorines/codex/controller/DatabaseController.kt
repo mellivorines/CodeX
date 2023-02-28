4c92118daeb889634deab6aef1d14d8b937b4dd1
mellivorines <68333104+mellivorines@users.noreply.github.com>
2023-02-28 19:53:56 +0800

    Create DatabaseController.kt

---
+++ codex-service/src/main/kotlin/com/mellivorines/codex/controller/DatabaseController.kt
@@ -0,0 +1,28 @@
+package com.mellivorines.codex.controller
+
+import com.mellivorines.codex.database.DatabaseService
+import com.mellivorines.codex.model.FieldInfo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import javax.sql.DataSource
+
+@RestController
+@RequestMapping("/api")
+class DatabaseController(private var databaseService: DatabaseService) {
+
+    @Autowired
+    lateinit var dataSource: DataSource
+
+    @GetMapping("/tableField")
+    fun getTableField(@RequestParam("table")table:String): List<FieldInfo>? {
+        return databaseService.getTableFields(table, dataSource)
+    }
+
+    @GetMapping("/allTable")
+    fun getAllTable(): List<String>? {
+        return databaseService.getAllTables(dataSource)
+    }
+}
