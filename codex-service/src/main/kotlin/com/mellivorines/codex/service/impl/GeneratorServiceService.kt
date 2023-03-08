1049514b571e63d7340e38f9dd1cb0e97716d76a
mellivorines <68333104+mellivorines@users.noreply.github.com>
2023-03-08 17:45:33 +0800

    Update GeneratorServiceService.kt

--- codex-service/src/main/kotlin/com/mellivorines/codex/service/impl/GeneratorServiceService.kt
+++ codex-service/src/main/kotlin/com/mellivorines/codex/service/impl/GeneratorServiceService.kt
@@ -1,28 +1,146 @@
 package com.mellivorines.codex.service.impl
 
-import com.mellivorines.codex.model.TableField
+import com.mellivorines.codex.SystemUtil
+import com.mellivorines.codex.constants.CommonConstant
+import com.mellivorines.codex.model.database.Table
+import com.mellivorines.codex.model.template.TemplateInfo
+import com.mellivorines.codex.model.template.Templates
+import com.mellivorines.codex.model.project.Project
+import com.mellivorines.codex.model.type.TypeMappings
 import com.mellivorines.codex.service.GeneratorService
-import com.mellivorines.codex.utils.TemplateUtils.getTemplateFromJson
+import com.mellivorines.codex.utils.TemplateUtils
+import com.mellivorines.codex.utils.TemplateUtils.getListFromJson
 import org.springframework.stereotype.Service
+import java.io.File
+import java.util.regex.Pattern
+import kotlin.io.path.Path
 
 @Service
 class GeneratorServiceService(private var databaseService: DatabaseService) :
     GeneratorService {
 
-    override fun generateModule(): List<TableField> {
-        var allTableField = ArrayList<TableField>()
-        var allTables = databaseService.getAllTables()
-        if (allTables != null) {
+    override fun generateModule(language: String, module: String?): List<Table>? {
+        var allTables = fillingTableInfo(databaseService.getAllTables())
+        generateModule(language, module, allTables)
+        return allTables
+
+    }
+
+    /**
+     * 生成模块
+     */
+    fun generateModule(language: String, module: String?, allTables: List<Table>?) {
+        var dir = if (language == CommonConstant.LANGUAGE_KOTLIN) {
+            CommonConstant.DIR_KOTLIN
+        } else {
+            CommonConstant.DIR_JAVA
+        }
+        var templatesInfo = if (language == CommonConstant.LANGUAGE_KOTLIN) {
+            getTemplate()?.kotlin
+        } else {
+            getTemplate()?.java
+        }
+        var basePath = SystemUtil.getBasePath(dir)
+        var project = getProject2JavaType()
+        if (allTables != null && project != null) {
+            for (table in allTables)
+                gen(basePath, module, table, project,templatesInfo)
+        }
+    }
+
+    fun gen(basePath: String, module: String?, table: Table, project: Project, templatesInfo: List<TemplateInfo>?) {
+        var tableFields = databaseService.getTableFields(table.tableName)
+        if (tableFields != null) {
+            if (tableFields.isNotEmpty()) {
+                val data: HashMap<String, Any> = fillingProjectInfo(module, table, project)
+                if (templatesInfo != null) {
+                    for (temp in templatesInfo) {
+                        var createOutFilePath = createOutFilePath(basePath, module, temp.outPath, table.className)
+                        var outFilePath = Path(createOutFilePath)
+                        var templatePath = Path(CommonConstant.DIR_RESOURCE + temp.templateName)
+
+                        TemplateUtils.render(outFilePath, templatePath, data)
+                    }
+                }
+
+            }
+        }
+    }
+
+    /**
+     * g构建文件输出路径
+     */
+    fun createOutFilePath(basedir: String, module: String?, outPath: String, className: String): String {
+        var basePath = if (module != null) {
+            basedir + File.separator + module
+        } else {
+            basedir
+        }
+
+        return basePath + File.separator + outPath.replace("{className}", className);
+    }
+
+    /**
+     * 填充项目信息
+     */
+    fun fillingProjectInfo(module: String?, table: Table, project: Project): HashMap<String, Any> {
+        val data: HashMap<String, Any> = HashMap()
+        project.packageName?.let { data.put("packageName", it) }
+        project.version?.let { data.put("version", it) }
+        table.className?.let { data.put("ClassName", it) }
+        table.tableComment?.let { data.put("tableComment", it) }
+        table.tableName?.let { data.put("tableName", it) }
+        module?.let { data.put("moduleName", it) }
+        project.creator.author?.let { data.put("author", it) }
+        project.creator.email?.let { data.put("email", it) }
+        project.creator.phone?.let { data.put("phone", it) }
+        table.date?.let { data.put("date", it) }
+        table.tableField?.let { data.put("fieldList", it) }
+        return data;
+    }
+
+    /**
+     * 填充缺失的信息
+     */
+    fun fillingTableInfo(allTables: List<Table>?): List<Table>? {
+        var dbType2JavaType = getDBType2JavaType()
+        if (allTables != null && dbType2JavaType != null) {
             for (table in allTables) {
-                var tableFields = databaseService.getTableFields(table.tableName)
-                if (tableFields != null) {
-                    allTableField.addAll(tableFields)
+                var tableField = table.tableField
+                if (tableField != null) {
+                    for (feild in tableField) {
+                        for (type in dbType2JavaType.typeMappings) {
+                            if (Pattern.matches(type.column, feild.fieldType)) {
+                                feild.fieldJavaType = type.java
+                                feild.fieldKotlinType = type.kotlin
+                            }
+                        }
+                    }
+
                 }
             }
         }
-        var templateFromJson = getTemplateFromJson("/template/template.json")
-        println(templateFromJson)
-        return allTableField
+        return allTables
+    }
 
+    /**
+     * 获取模板信息
+     */
+    fun getTemplate(): Templates? {
+        return getListFromJson("/template/template.json")
+    }
+
+    /**
+     * 获取数据类型映射信息
+     */
+    fun getDBType2JavaType(): TypeMappings? {
+        return getListFromJson("/template/DBType.json")
+    }
+
+    /**
+     * 获取项目相关配置
+     */
+    fun getProject2JavaType(): Project? {
+        return getListFromJson("/template/project.json")
     }
 }
