ddf1af61c8f7bb559e4d0baebc39c7f7ac354c10
mellivorines <68333104+mellivorines@users.noreply.github.com>
2023-03-08 17:55:42 +0800

    Delete TableField.kt

--- codex-service/src/main/kotlin/com/mellivorines/codex/model/TableField.kt
+++
@@ -1,9 +0,0 @@
-package com.mellivorines.codex.model
-
-data class TableField(
-    var tableName:String,
-    var fieldName: String,
-    var fieldComment: String,
-    var fieldType: String,
-    var fieldSize: Int
-)
