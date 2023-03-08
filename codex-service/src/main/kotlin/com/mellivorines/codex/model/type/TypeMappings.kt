d643dc8f40e82a6ec8b440ed72cf0f977193d4c5
mellivorines <68333104+mellivorines@users.noreply.github.com>
2023-03-08 17:49:52 +0800

    Create TypeMappings.kt

---
+++ codex-service/src/main/kotlin/com/mellivorines/codex/model/type/TypeMappings.kt
@@ -0,0 +1,11 @@
+package com.mellivorines.codex.model.type
+
+data class TypeMappings(
+    var typeMappings: List<Type>
+)
+
+data class Type(
+    var column: String,
+    var java: String,
+    var kotlin: String
+)
